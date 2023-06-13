a8f68981951c9d85b51a24cc7859f1d224714bb7
Douglas Oliveira <doglasolivera13@gmail.com>
2023-06-13 17:36:35 -0300

    Criação da rota /teams, que retorna o array de times utilizando o método GET

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPFC',
+  },
+  {
+    id: 2,
+    name: 'Grêmio Foot-ball Porto Alegrense',
+    initials: 'GRÊ',
+  },
+];
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 module.exports = app;
